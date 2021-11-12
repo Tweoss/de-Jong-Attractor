8282ad52ed8efcbd970a3a56aebb1e3f6c304322
Francis Chua <francis@opennetworking.org>
2021-11-11 18:30:25 -0800

    Initial Commit

---
+++ de Jong Attractor/de_Jong_AttractorView.h
@@ -0,0 +1,12 @@
+//
+//  de_Jong_AttractorView.h
+//  de Jong Attractor
+//
+//  Created by Francis Chua on 11/11/21.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface de_Jong_AttractorView : ScreenSaverView
+
+@end
