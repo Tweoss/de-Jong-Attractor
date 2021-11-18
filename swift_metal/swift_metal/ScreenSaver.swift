49eb83c69e5a297138cd5ca8a9f9acf6e59f3f50
Francis Chua <francis@opennetworking.org>
2021-11-17 18:49:15 -0800

    working vertex transform

--- swift_metal/swift_metal/ScreenSaver.swift
+++ swift_metal/swift_metal/ScreenSaver.swift
@@ -10,6 +10,10 @@ import Foundation
 import Metal
 import MetalKit
 
+struct ColoredPoint {
+    var coord: SIMD2<Float>;
+    var color: SIMD3<Float>;
+};
 
 class Main: ScreenSaverView, MTKViewDelegate {
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
@@ -19,22 +23,38 @@ class Main: ScreenSaverView, MTKViewDelegate {
     func draw(in view: MTKView) {
         let drawable: CAMetalDrawable = self.metalView.currentDrawable!;
         let buffer: MTLCommandBuffer = self.commandQueue.makeCommandBuffer()!;
-        let encoder: MTLComputeCommandEncoder = buffer.makeComputeCommandEncoder()!;
         
-        encoder.setComputePipelineState(self.computeState);
+        let encoder_2: MTLComputeCommandEncoder = buffer.makeComputeCommandEncoder()!;
+        encoder_2.setComputePipelineState(self.fillState);
+        encoder_2.setTexture(drawable.texture, index: 0);
+        let fill_groups: MTLSize = MTLSizeMake(8,8,1);
+        let fill_threadPerGroup: MTLSize = MTLSizeMake(drawable.texture.width / fill_groups.width, drawable.texture.height / fill_groups.height, fill_groups.depth);
+        encoder_2.dispatchThreadgroups(fill_threadPerGroup, threadsPerThreadgroup: fill_groups);
+        encoder_2.endEncoding();
+        
+        
+        let encoder: MTLComputeCommandEncoder = buffer.makeComputeCommandEncoder()!; 
+        
+        // Vertex buffer
+        let dataSize = vertexData.count*MemoryLayout<ColoredPoint>.stride
+        let vertexBuffer = self.metalView.device?.makeBuffer(bytes: vertexData, length: dataSize, options: [])!
+        
+        encoder.setComputePipelineState(self.transformState);
         encoder.setTexture(drawable.texture, index: 0);
-        encoder.setBytes(&self.time, length: MemoryLayout.size(ofValue: self.time), index: 0);
-        encoder.setBytes(&self.a, length: MemoryLayout.size(ofValue: self.a), index: 1);
-        encoder.setBytes(&self.b, length: MemoryLayout.size(ofValue: self.b), index: 2);
-        encoder.setBytes(&self.c, length: MemoryLayout.size(ofValue: self.c), index: 3);
-        encoder.setBytes(&self.d, length: MemoryLayout.size(ofValue: self.d), index: 4);
-        let groups: MTLSize = MTLSizeMake(8,8,1);
-        let threadPerGroup = MTLSizeMake(drawable.texture.width / groups.width, drawable.texture.height / groups.height, groups.depth);
+        encoder.setBuffer(vertexBuffer, offset: 0, index: 0);
+        encoder.setBytes(&self.time, length: MemoryLayout.size(ofValue: self.time), index: 1);
+        encoder.setBytes(&self.a, length: MemoryLayout.size(ofValue: self.a), index: 2);
+        encoder.setBytes(&self.b, length: MemoryLayout.size(ofValue: self.b), index: 3);
+        encoder.setBytes(&self.c, length: MemoryLayout.size(ofValue: self.c), index: 4);
+        encoder.setBytes(&self.d, length: MemoryLayout.size(ofValue: self.d), index: 5);
+        let groups: MTLSize = MTLSizeMake(4,1,1);
+        let threadPerGroup = MTLSize(width: vertexBuffer!.length / groups.width, height: 1, depth: 1);
         encoder.dispatchThreadgroups(threadPerGroup, threadsPerThreadgroup: groups);
         encoder.endEncoding();
+        
         buffer.present(drawable);
         buffer.commit();
-        
+
         self.time += self.timespeed;
         self.a = -2.0 + sin(time);
         self.b = -2.0 + sin(time / 120);
@@ -45,12 +65,15 @@ class Main: ScreenSaverView, MTKViewDelegate {
     
     
     var metalView: MTKView!
-    var computeState: MTLComputePipelineState!
+    var transformState: MTLComputePipelineState!
+    var fillState: MTLComputePipelineState!
     var library: MTLLibrary!
     var commandQueue: MTLCommandQueue!
     var time: Float = fmod(Float(CFAbsoluteTimeGetCurrent()), 86400.0)
     var timespeed: Float = Float.pi / 360.0 / 2.0
     var a: Float = -2.0 + sin(fmod(Float(CFAbsoluteTimeGetCurrent()), 86400.0)), b: Float = -2.0 + sin(fmod(Float(CFAbsoluteTimeGetCurrent()), 86400.0)/120), c: Float = -1.2 + sin(fmod(Float(CFAbsoluteTimeGetCurrent()), 86400.0)/360), d: Float = 2.0 + sin(fmod(Float(CFAbsoluteTimeGetCurrent()), 86400.0)/720)
+    
+    var vertexData: Array<ColoredPoint> = (0..<100).map { _ in ColoredPoint(coord: SIMD2<Float>(Float.random(in: -1.0...1.0), Float.random(in: -1.0...1.0)), color: SIMD3<Float>(0.0, 0.0, 0.0)) }
     
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
@@ -60,15 +83,19 @@ class Main: ScreenSaverView, MTKViewDelegate {
         self.metalView.delegate = self;
         self.addSubview(self.metalView)
         try! self.library = self.metalView.device?.makeDefaultLibrary(bundle: Bundle(for: type(of: self)) )
-        let compute = self.library.makeFunction(name: "compute_function")
-        self.computeState = try! self.metalView.device!.makeComputePipelineState(function: compute!)
+        let fill_black = self.library.makeFunction(name: "fill_black")
+        self.fillState = try!
+        self.metalView.device!
+            .makeComputePipelineState(function: fill_black!);
+        let transform = self.library.makeFunction(name: "transform_function")
+        self.transformState = try! self.metalView.device!.makeComputePipelineState(function: transform!)
         self.commandQueue =  self.metalView.device?.makeCommandQueue()
     }
     
     required init?(coder decoder: NSCoder) {
         super.init(coder: decoder)
     }
-
+    
     override func animateOneFrame() {
         
         
@@ -77,7 +104,7 @@ class Main: ScreenSaverView, MTKViewDelegate {
     func clearStage() {
     }
     
-
+    
     override func draw(_ rect: NSRect) {
     }
 }
--- swift_metal/swift_metal/shader.metal
+++ swift_metal/swift_metal/shader.metal
@@ -91,3 +91,40 @@ kernel void compute_function(texture2d<float, access::write> texture [[texture(0
             texture.write(float4(color_point.color, opacity), uint2(x,y));
         }
 }
+
+
+
+
+// Metal side
+struct Vertex {
+    float2 position;
+    float3 color;
+};
+
+kernel void transform_function(
+                               texture2d<float, access::write> texture [[texture(0)]],
+                               device ColoredPoint *buffer [[ buffer(0) ]],
+                               uint2 vid [[ thread_position_in_grid ]],
+                               device const float &time [[buffer(1)]], device const float &a [[buffer(2)]], device const float &b [[buffer(3)]], device const float &c [[buffer(4)]], device const float &d [[buffer(5)]]
+                               )
+{
+    
+    ColoredPoint color_point = transformPoint(buffer[vid.x].coord.x, buffer[vid.x].coord.y, a, b, c, d, texture.get_width(), texture.get_height(), time);
+    float opacity = 1.0;
+    uint x = color_point.coord[0] / 3 * texture.get_width() + texture.get_width() / 2;
+    uint y = color_point.coord[1] / 3 * texture.get_height() + texture.get_height() / 2;
+    texture.write(float4(color_point.color, opacity), uint2(x,y));
+    buffer[vid.x] = color_point;
+}
+
+kernel void fill_black(
+                               texture2d<float, access::write> texture [[texture(0)]]
+                       , uint2 gid [[thread_position_in_grid]]
+                               )
+{
+    
+    texture.write(float4(0.0, 0.0, 0.0, 0.0), gid);
+}
+
+
+
